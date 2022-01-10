93ae63678843b3ca1671ff4769c7f41916c7d54f
Breezy <1147159386@qq.com>
2022-01-10 16:46:50 +0800

    deploy

---
+++ precache-manifest.de127dcbd7a53f841812938ab2365d16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196ca9c4870122662618",
+    "url": "/simple-money-website/css/app.de7a49fa.css"
+  },
+  {
+    "revision": "dbb4cd0973cf52a0021a6286c0775f19",
+    "url": "/simple-money-website/index.html"
+  },
+  {
+    "revision": "196ca9c4870122662618",
+    "url": "/simple-money-website/js/app.5d6111a4.js"
+  },
+  {
+    "revision": "924563aa5d70d4d5e7f7",
+    "url": "/simple-money-website/js/chunk-vendors.02438c8a.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/simple-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simple-money-website/robots.txt"
+  }
+]);
